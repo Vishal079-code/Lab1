e7439b26866dc122b81daaa2726198c0dbcfc277
Vishal079-code <u24ai079@aid.svnit.ac.in>
2024-09-14 12:48:16 +0530

    Update practical14.c

--- practical14.c
+++ practical14.c
@@ -6,33 +6,13 @@ int main()
 	scanf("%c",&c);
 	printf("so by the given data the characterASCII code is %d\n",c);
 	 if (c>96)
-	 {
 	 	printf("the given character is a small letter");
-	 	
-	 }
 	 else if (c>64)
-	 {
-	 	printf("the given character is a capital letter ");
-	 	
-	 	
-	 }
+                printf("the given character is a capital letter ");
 	else if (c>47)
-	 {
 	 	printf("the given character is a digit ");
-	 	
-	 	
-	 }
-	
 	else if (c>32)
-	 {
-	 	printf("the given character is a symbol  ");
-	 	
-	 	
-	 }
+	        printf("the given character is a symbol  ");
 	 else
-	 {
-	 	printf("sorry you have entered the wrong value");
-	 }
-	 
-	
+	 	printf("sorry you have entered the wrong value");	
 }
